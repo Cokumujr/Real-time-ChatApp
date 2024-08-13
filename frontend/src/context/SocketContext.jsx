2471c97898ef2093dee625611feedcad04484517
Cokumujr <cokumujnr@gmail.com>
2024-08-13 23:34:20 +0300

    preparing for render deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
     useEffect(() => {
 
         if (authUser) {
-           const socket = io('https://chat-jyuce.vercel.app',{
+           const socket = io('http://localhost:5000',{
             query:{
                 userId: authUser._id,
              }});
