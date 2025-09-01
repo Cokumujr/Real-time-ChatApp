bf2f57f13e9d829eb15fd6c6389d7e4847f80c1e
Cokumu Junior <cokumujnr@gmail.com>
2025-09-01 17:03:04 +0300

    Add /ping route for keep-alive

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -9,11 +10,11 @@ const authRoutes = require('./routes/auth.routes');
 const messageRoutes = require('./routes/message.routes');
 const userRoutes = require('./routes/user.routes');
 
-const connectdb = require('./db/connectdb'); 
-const {app,server} = require('./socket/socketio');
+const connectdb = require('./db/connectdb');
+const { app, server } = require('./socket/socketio');
 
 
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -25,9 +26,9 @@ app.use(cors());
 // Routes
 
 
-app.use ("/api/auth", authRoutes);
-app.use ("/api/messages", messageRoutes);
-app.use ("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 // Adjust the path to point to the correct location of your frontend build
@@ -35,6 +36,17 @@ const frontendBuildPath = path.join(__dirname, '..', 'frontend', 'dist');
 
 // Serve static files from the frontend build directory
 app.use(express.static(frontendBuildPath));
+
+
+app.get("/ping", async (req, res) => {
+  try {
+    await mongoose.connection.db.admin().ping();
+    res.status(200).json({ message: " Pong! MongoDB is awake." });
+  } catch (err) {
+    console.error("Ping failed:", err);
+    res.status(500).json({ message: " Ping failed", error: err.message });
+  }
+});
 
 // Handle other routes
 app.get('*', (req, res) => {
@@ -48,6 +60,17 @@ console.log('Index.html path:', path.join(frontendBuildPath, 'index.html'));
 
 // start server
 server.listen(PORT, () => {
-    connectdb(); 
-    console.log(`Server running on port ${PORT}`);
+  connectdb();
+  console.log(`Server running on port ${PORT}`);
+
+  // self-ping 
+  setInterval(async () => {
+    if (mongoose.connection.readyState === 1) {
+      try {
+        await mongoose.connection.db.admin().ping();
+      } catch (e) {
+        console.error("Self ping error:", e?.message);
+      }
+    }
+  }, 5 * 60 * 1000);
 });
